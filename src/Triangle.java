0eca4a6ff3d4709a14e280cb715ca8beb43b08b7
Notnik_kg <aliaskartemirbekov@gmail.com>
2023-11-02 21:52:56 +0300

    MyCLASS2

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,18 +1,18 @@
-public class Triangle {
-
-    int a;
-    int b;
-    int c;
-
-    public Triangle(int a, int b , int c){
-        this.a = a;
-        this.b = b;
-        this.c = c;
-    }
-
-    public int area(){
-
-        System.out.println(" Summa  :" + (a * b * c) /2);
-        return 0;
-    }
-}
+///public class Triangle {
+//
+//    int a;
+//    int b;
+//    int c;
+//
+//    public Triangle(int a, int b , int c){
+//        this.a = a;
+//        this.b = b;
+//        this.c = c;
+//    }
+//
+//    public int area(){
+//
+//        System.out.println(" Summa  :" + (a * b * c) /2);
+//        return 0;
+//    }
+//}
